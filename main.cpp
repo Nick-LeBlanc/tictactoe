5cce9b0aa7e6fca7f123e03e8e7e7e659ccb7e2d
Nick-LeBlanc <97707415+Nick-LeBlanc@users.noreply.github.com>
2022-08-10 09:08:27 -0500

    SFML Template

--- main.cpp
+++ main.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
-#include "Player.h"
-#include "Walls.h"
+
 
 
 int main()
 {
-    //player box
-    int height = 480, width = 640;
 
-
-
-    //setup window
-    sf::RenderWindow window(sf::VideoMode(width, height), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(false);
 
-    //init player and walls
-    Player Player(sf::Color::Green, 20.f);
-    Walls Walls;
-    
-    //game loop
+
     while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
@@ -28,25 +18,13 @@ int main()
                 window.close();
         }
 
-        //game logic
-        //player movement -- he jumps
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            Player.setVelocity(-10.f);
+        //logic here
 
-        //wall movement
-
-
-        //clear screen
         window.clear();
-
-        Player.update(window);
-        Walls.update(window);
         
+        //draw things here
 
-        //display things
         window.display();
-
-
     }
 
     return 0;
